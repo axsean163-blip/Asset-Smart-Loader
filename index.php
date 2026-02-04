4593791cb20698ef6a8e0bf527c07fe4133276f9
axsean163-blip <axsean163@gmail.com>
2026-02-03 18:27:57 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include "smart-auto-loader.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Smart Auto Loader Demo</title>
+    <?php load_assets(); ?>
+</head>
+
+<body>
+
+<h1>Smart Auto Loader Test Page</h1>
+
+<p>This page automatically loads assets based on URL.</p>
+
+</body>
+</html>
